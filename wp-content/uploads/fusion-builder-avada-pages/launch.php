15552d0433ea71fef3fe310d419d08cc31341433
admin <surumodak1996@gmail.com>
2021-03-31 07:08:11 +0000

    Bush-Safe edit 1

---
+++ wp-content/uploads/fusion-builder-avada-pages/launch.php
@@ -0,0 +1,98 @@
+<?php
+
+		// Exit if accessed directly
+		if ( ! defined( 'ABSPATH' ) ) {
+			exit;
+		}
+
+		function fusion_builder_add_launch_demo( $demos ) {
+
+		$demos['launch'] = array (
+  'category' => 'Avada Launch',
+  'pages' => 
+  array (
+     
+    array (
+      'name' => 'Launch Home',
+      'link' => 'https://avada.theme-fusion.com/launch/',
+      'page_template' => 'blank.php',
+      'content' => '[fusion_builder_container hundred_percent="no" hundred_percent_height="yes" hundred_percent_height_scroll="no" hundred_percent_height_center_content="yes" equal_height_columns="no" hide_on_mobile="small-visibility,medium-visibility,large-visibility" background_image="https://placehold.it/2400x1594" background_position="center center" background_repeat="no-repeat" fade="no" background_parallax="none" enable_mobile="no" parallax_speed="0.3" video_aspect_ratio="16:9" video_loop="yes" video_mute="yes" border_style="solid" padding_top="100px" padding_bottom="100px" align_content="center" flex_column_spacing="0px" type="flex"][fusion_builder_row][fusion_builder_column type="1_1" layout="1_1" spacing="" center_content="no" link="" target="_self" min_height="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" background_color="" background_image="" background_position="left top" background_repeat="no-repeat" hover_type="none" border_color="" border_style="solid" border_position="all" padding_top="" padding_right="" padding_bottom="" padding_left="" margin_top="" margin_bottom="0" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset="" last="true" border_sizes_top="0px" border_sizes_bottom="0px" border_sizes_left="0px" border_sizes_right="0px" first="true"][fusion_imageframe image_id="98" style_type="none" stylecolor="" hover_type="none" bordersize="" bordercolor="" borderradius="" align="center" lightbox="no" gallery_id="" lightbox_image="" alt="" link="https://bit.ly/2l3jd8A" linktarget="_blank" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset=""]https://placehold.it/400x129[/fusion_imageframe][/fusion_builder_column][fusion_builder_column type="1_1" layout="1_1" spacing="" center_content="no" link="" target="_self" min_height="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" background_color="" background_image="" background_position="left top" undefined="" background_repeat="no-repeat" hover_type="none" border_color="" border_style="solid" border_position="all" padding_top="" padding_right="10%" padding_bottom="" padding_left="10%" margin_top="" margin_bottom="20px" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset="" last="true" border_sizes_top="0px" border_sizes_bottom="0px" border_sizes_left="0px" border_sizes_right="0px" first="true" margin_bottom_small="24px"][fusion_text]
+<h1 style="text-align: center; text-shadow: #000 1px 0 40px;">We are excited to bring you months of dedicated work. Signup below to find out more.</h1>
+[/fusion_text][/fusion_builder_column][fusion_builder_column type="1_1" layout="1_1" spacing="" center_content="yes" link="" target="_self" min_height="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" background_color="" background_image="" background_position="left top" undefined="" background_repeat="no-repeat" hover_type="none" border_color="" border_style="solid" border_position="all" padding_top="" padding_right="20%" padding_bottom="" padding_left="20%" margin_top="" margin_bottom="10px" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset="" last="true" align_content="center" border_sizes_top="0px" border_sizes_bottom="0px" border_sizes_left="0px" border_sizes_right="0px" first="true" padding_left_medium="48px" padding_right_medium="48px" spacing_left_small="" padding_right_small="20px" padding_left_small="20px"][fusion_form form_post_id="124" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" /][/fusion_builder_column][fusion_builder_column type="1_1" layout="1_1" spacing="" center_content="no" link="" target="_self" min_height="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" background_color="" background_image="" background_position="left top" background_repeat="no-repeat" hover_type="none" border_color="" border_style="solid" border_position="all" padding_top="" padding_right="" padding_bottom="" padding_left="" margin_top="" margin_bottom="" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset="" last="true" border_sizes_top="0px" border_sizes_bottom="0px" border_sizes_left="0px" border_sizes_right="0px" first="true"][fusion_countdown countdown_end="2021-04-01 12:30:00" timezone="" layout="" show_weeks="" label_position="" border_radius="" heading_text="" subheading_text="" link_url="https://bit.ly/2l3jd8A" link_text="" link_target="default" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" background_color="" background_image="" background_position="" background_repeat="" counter_box_spacing="" counter_box_color="" counter_border_size="" counter_border_color="" counter_border_radius="" counter_padding_top="" counter_padding_right="" counter_padding_bottom="" counter_padding_left="" counter_font_size="" counter_text_color="" label_font_size="" label_color="" heading_font_size="" heading_text_color="" subheading_font_size="" subheading_text_color="" link_text_color="" /][fusion_separator style_type="none" flex_grow="0" alignment="center" border_size="" sep_color="" icon="" icon_size="" icon_color="" icon_circle="" icon_circle_color="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" sticky_display="normal,sticky" class="" id="" top_margin="20px" bottom_margin="15px" /][fusion_text fusion_font_variant_text_font=""]
+<p style="text-align: center;">© Copyright 2012 - <script>document.write(new Date().getFullYear());</script>   |   Avada Theme by <a href="http://theme-fusion.com" target="_blank" rel="noopener noreferrer">Theme Fusion</a>   |   All Rights Reserved   |   Powered by <a href="http://wordpress.org" target="_blank" rel="noopener noreferrer">WordPress</a></p>
+[/fusion_text][/fusion_builder_column][/fusion_builder_row][/fusion_builder_container]',
+      'meta' => 
+      array (
+        'pyre_demo_slider' => '',
+        'pyre_slider_type' => 'no',
+        'pyre_slider' => '0',
+        'pyre_wooslider' => '0',
+        'pyre_revslider' => '0',
+        'pyre_elasticslider' => '0',
+        'pyre_slider_position' => 'default',
+        'pyre_avada_rev_styles' => 'default',
+        'pyre_fallback' => '',
+        'pyre_main_top_padding' => '',
+        'pyre_main_bottom_padding' => '',
+        'pyre_hundredp_padding' => '',
+        'pyre_show_first_featured_image' => 'yes',
+        'pyre_display_header' => 'no',
+        'pyre_header_100_width' => 'default',
+        'pyre_header_bg_color' => '',
+        'pyre_header_bg_opacity' => '',
+        'pyre_header_bg' => '',
+        'pyre_header_bg_full' => 'no',
+        'pyre_header_bg_repeat' => 'repeat',
+        'pyre_displayed_menu' => 'default',
+        'pyre_display_footer' => 'no',
+        'pyre_display_copyright' => 'default',
+        'pyre_footer_100_width' => 'default',
+        'pyre_sidebar_position' => 'default',
+        'pyre_sidebar_sticky' => 'default',
+        'pyre_sidebar_bg_color' => '',
+        'pyre_page_bg_layout' => 'default',
+        'pyre_page_bg_color' => '',
+        'pyre_page_bg' => '',
+        'pyre_page_bg_full' => 'no',
+        'pyre_page_bg_repeat' => 'repeat',
+        'pyre_wide_page_bg_color' => '',
+        'pyre_wide_page_bg' => '',
+        'pyre_wide_page_bg_full' => 'yes',
+        'pyre_wide_page_bg_repeat' => 'no-repeat',
+        'pyre_page_title' => 'default',
+        'pyre_page_title_breadcrumbs_search_bar' => 'default',
+        'pyre_page_title_text' => 'default',
+        'pyre_page_title_text_alignment' => 'default',
+        'pyre_page_title_custom_text' => '',
+        'pyre_page_title_text_size' => '',
+        'pyre_page_title_line_height' => '',
+        'pyre_page_title_custom_subheader' => '',
+        'pyre_page_title_custom_subheader_text_size' => '',
+        'pyre_page_title_font_color' => '',
+        'pyre_page_title_100_width' => 'default',
+        'pyre_page_title_height' => '',
+        'pyre_page_title_mobile_height' => '',
+        'pyre_page_title_bar_bg_color' => '',
+        'pyre_page_title_bar_borders_color' => '',
+        'pyre_page_title_bar_bg' => '',
+        'pyre_page_title_bar_bg_retina' => '',
+        'pyre_page_title_bar_bg_full' => 'default',
+        'pyre_page_title_bg_parallax' => 'default',
+        'pyre_fallback_id' => '',
+        'pyre_combined_header_bg_color' => '',
+        'pyre_mobile_header_bg_color' => '',
+        'pyre_header_bg_id' => '',
+        'pyre_responsive_sidebar_order' => '',
+        'pyre_page_bg_id' => '',
+        'pyre_wide_page_bg_id' => '',
+        'pyre_page_title_subheader_font_color' => '',
+        'pyre_page_title_bar_bg_id' => '',
+        'pyre_page_title_bar_bg_retina_id' => '',
+      ),
+    ),
+  ),
+);
+
+			return $demos;
+		}
+		add_filter( 'fusion_builder_get_demo_pages', 'fusion_builder_add_launch_demo' );
